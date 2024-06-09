5c9fd6759ca0e99767494674f1f3d0672295669c
jakepalmer <jpalmer515@gmail.com>
2024-06-08 18:16:49 -0600

    Moved files to their respective folders

--- tempCodeRunnerFile.java
+++
@@ -1,9 +0,0 @@
-     System.out.println("\nBAD NEWS CAR SALES\n");
-        System.out.println("Select from the following options:");
-        System.out.println("----------------------------------");
-        System.out.println("1. ADD VEHICLE");
-        System.out.println("2. UPDATE VEHICLE");
-        System.out.println("3. REMOVE VEHICLE");
-        System.out.println("4. DISPLAY INVENTORY");
-        System.out.println("5. EXPORT INVENTORY");
-        System.out.println("6. EXIT");
